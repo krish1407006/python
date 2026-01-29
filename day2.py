f5fd11a7c046fca350e61783cf145b1640c52fbf
krish1407006 <krish14072006@gmail.com>
2026-01-29 20:09:05 +0530

    python

---
+++ day2.py
@@ -0,0 +1,16 @@
+a= 1234567
+print (a)
+b= 1.234567
+print (b)
+print("This is day 2 of learning Python!")
+print(a+b)
+c = True
+print("this typ of a is" , type(a))
+print("this typ of b is" , type(b))
+print("this typ of c is" , type(c))
+
+list =[ [ 8.2 , 3 , 5] , ["krish" , "is" , "learning"]]
+print (list)
+
+tuple = ( 8.2 , 3 , 5 , "krish" , "is" , "learning")
+print (tuple)
