a21fbf98a52e7b45fbba7e719e9a02e6554b3315
krish1407006 <krish14072006@gmail.com>
2026-03-28 12:21:20 +0530

    day4

---
+++ day3.py
@@ -0,0 +1,10 @@
+print(5+8)
+print(5-8)
+print(5*8)
+print(8/5)
+print(8//5)
+print(8%5)
+print(5**3)
+print("This is day 3 of learning Python!")
+
+
---
+++ day4.py
@@ -0,0 +1,15 @@
+# type casting example
+# In this example, we have two string variables 'a' and 'b'. We will first concatenate them as strings and then convert them to integers to perform addition.
+a = "1"
+b = " 12"
+print(a + b)  # This will concatenate the two strings, resulting in "1 12"
+print(int(a) + int(b))  # This will convert the strings to integers and then add them, resulting in 13
+
+#input example
+
+a = input("enter a number : ")
+b = input("enter another number : ")
+print("the number a and b are", a ,b)
+
+print ( int (a) + int (b))  # This will convert the input strings to integers and then add them together
+print (int (a) * int (b))  # This will convert the input strings to integers and then multiply them together    
