284128a85941394eb1c6f4ce9978b7996dd13c5a
krish1407006 <krish14072006@gmail.com>
2026-04-11 17:02:01 +0530

    day11

---
+++ day11.py
@@ -0,0 +1,14 @@
+# break and continue statements
+
+for i in range (20):
+    print (" 5 X " , i+1 , "=" , 5 *  (i+1))
+
+    if (i == 10):
+        print("This is the end of the loop")
+        break 
+    elif (i == 5):
+        print("This is the end of the loop")
+    continue
+    print("This is the end of the loop")
+
+
