beccb405276170987538bc390f5e30cda9fa364c
krish1407006 <krish14072006@gmail.com>
2026-04-12 14:33:22 +0530

    day12

--- day12.py
+++ day12.py
@@ -4,13 +4,24 @@ def gmean (a, b):
      mean = (a*b)/(a+b)
      print(mean)
 
+def isgreater (a, b):
+  if(a > b):
+    print("first is greater than second")
+  elif (a < b):
+    print("second is less than first")
+
 a = 7
 b = 9
 gmean(a, b)
-
+isgreater(a, b)
 c = 10
 d = 11
 gmean(c, d)
+isgreater(c, d)
+ 
+  
 
-def isgreater (a, b):
-  pass 
+
+#all type of built in functiona are:
+# min(), max(), sum(), len(), sorted(), reversed(), range(), enumerate(), zip() etc.
+# user defined function
