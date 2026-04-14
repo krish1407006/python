6c7c65427de60878e5155fc0b9786078973bf82a
krish1407006 <krish14072006@gmail.com>
2026-04-14 13:49:14 +0530

    day16

---
+++ day16.py
@@ -0,0 +1,14 @@
+# tuple in python
+
+tup = (1, 2, 3, 4, 5, "krish", True )
+
+print(tup)
+
+print(type(tup), tup)
+
+# the tuple is immutable we cannot change the value of tuple
+# tup[0] = 100
+
+tup2 = tup[0:4]
+print(tup2)
+
