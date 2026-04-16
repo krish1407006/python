2305caadf81544f9b18df885bbd89464ba0e317c
krish1407006 <krish14072006@gmail.com>
2026-04-16 22:55:54 +0530

    day17 day18

---
+++ day17.py
@@ -0,0 +1,12 @@
+#f strings in python
+
+country = input("Enter your country name : ")
+
+C = f"my country name is {country}"
+print(C)
+
+txt = 49.0000999
+D = f"the value of txt is {txt:.2f}"
+print(D)
+
+# we can use double curly braces to print the curly braces in the output
---
+++ day18.py
@@ -0,0 +1,12 @@
+# docstrings in python
+
+def  multiply(a, b):
+    '''    This function takes two numbers as input and returns their product.'''
+    print(a * b)
+
+multiply(5, 8)
+print(multiply.__doc__)  # This will print the docstring of the multiply function
+
+# PEP8 is a style guide for Python code that provides conventions for writing clean and readable code.
+#  It covers various aspects of coding style, including naming conventions, indentation, line length, and more.
+#  Following PEP8 helps improve the readability and maintainability of your code.
