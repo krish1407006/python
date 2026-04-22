4a684a77e266985f0fb8bd38a3483452a4c4f2f8
krish1407006 <krish14072006@gmail.com>
2026-04-22 19:03:11 +0530

    day23

---
+++ day23.py
@@ -0,0 +1,19 @@
+# loop with else in python
+
+# for i in range(6):
+#     print(i)
+#     if (i == 3):
+#      break
+
+# else:
+#     print("This is the end of the loop")
+
+
+i = 0
+while i<9:
+   i = i+ 1
+   print(i)
+else:
+    print("This is the end of the loop")
+
+
