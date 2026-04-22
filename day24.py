ed2cb4eac13a8a1fb852642b9092a7a48308972b
krish1407006 <krish14072006@gmail.com>
2026-04-22 19:57:02 +0530

    DAY24

--- day24.py
+++ day24.py
@@ -1,11 +1,22 @@
 # exceptional handling
 
-a = input("enter the number :")
+# a = input("enter the number :")
 
-print("multiplication table of {a} is :")
+# print("multiplication table of {a} is :")
+# try:
+#    for i in range(1, 20):
+#     print(f"{int(a)} X {i} = {int(a) * i}")
+# except:
+#   print("enter the valid integer")
+
+
 try:
-   for i in range(1, 20):
-    print(f"{int(a)} X {i} = {int(a) * i}")
-except:
-  print("enter the valid integer")
+ num = int(input("enter the number :"))
+ a = [10,3]
+ print(a[num])
 
+except ValueError:
+    print("enter the valid integer")
+    
+except IndexError:
+    print("index out of range")
