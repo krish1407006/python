c303b34e0ef94717c36f300f2c0de29a56b6448b
krish1407006 <krish14072006@gmail.com>
2026-04-23 14:04:38 +0530

    day25

--- day25.py
+++ day25.py
@@ -14,3 +14,7 @@ def funct():
 
 x = funct()
 print(x)
+
+
+#finally clause is used to execute some code no matter what happens in the try block.
+#  It is used to clean up resources or to perform some actions that must be executed regardless of whether an exception occurred or not.
