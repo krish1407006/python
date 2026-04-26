efc8961280b4082fceddf588a411987ea7ad009b
krish1407006 <krish14072006@gmail.com>
2026-04-26 16:49:26 +0530

    day8

--- day8.py
+++ day8.py
@@ -9,8 +9,8 @@ match x:
         print("x is 1")
     case 6:
         print("x is 6")
-    case _ if x > 50 :
-        print(x, "is greater than 50")
+    case _ if x >= 50 :
+        print(x, "is greater or = to  50")
     case _ if x != 90 :
         print(x, "is less than 50 and greater than 20")
               
