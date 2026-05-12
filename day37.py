f3aa3977b3ad3edd8483d90d401b8abf145e3255
krish1407006 <krish14072006@gmail.com>
2026-05-12 07:40:48 +0530

    day37

--- day37.py
+++ day37.py
@@ -17,5 +17,22 @@ decorator(add)(5, 3)
 
 
 
-#
+import logging
+
+logging.basicConfig(level=logging.INFO)
+
+def log_decorator(func):
+
+    def wrapper(*args, **kwargs):
+        logging.info(f"Function {func.__name__} is called with arguments: {args} and keyword arguments: {kwargs}")
+        result = func(*args, **kwargs)
+        logging.info(f"Function {func.__name__} returned: {result}")
+        return result
+    return wrapper
+
+@log_decorator
+def multiply(a, b):
+    return a * b
+result = multiply(4, 5)
+
 
