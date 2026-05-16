82b83840e6a01a3f1271ea7a49d4071694358a44
krish1407006 <krish14072006@gmail.com>
2026-05-16 23:30:50 +0530

    day45

---
+++ day45.py
@@ -0,0 +1,10 @@
+# Dic , Dir and help in python
+
+d = {"name": "krish", "age": 22, "city": "delhi"}
+print(dict)  # Output: <class 'dict'>
+print(d)  # Output: {'name': 'krish', 'age': 22, 'city': 'delhi'}
+print(dir(d))  # Output: ['__class__', '__contains__', '__del
+
+
+print(help(d))  # Output: Help on dict object
+
