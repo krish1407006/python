4be1bcc91ad8a0799280e4752c664a6687c3f852
krish1407006 <krish14072006@gmail.com>
2026-05-17 14:25:05 +0530

    day49

--- day49.py
+++ day49.py
@@ -1,1 +1,24 @@
-#
+# operator overloading in python
+
+
+class point:
+
+    def __init__(self,x,y):
+        self.x = x
+        self.y = y
+
+    def __add__(self,other):
+        return point((self.x + other.x),(self.y + other.y))
+
+p1 = point(2,3)
+p2 = point(4,5)
+
+p3 = p1 + p2  # this will call the __add__ method of the point class
+
+
+print("p1:",p1.x,p1.y)
+print("p2:",p2.x,p2.y)
+
+print("p3:",p3.x,p3.y)
+
+
