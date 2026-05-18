8a77bac3136333b5ee653fc67d76bb6ba241cda6
krish1407006 <krish14072006@gmail.com>
2026-05-18 14:12:09 +0530

    day54

---
+++ day54.py
@@ -0,0 +1,15 @@
+# Time module in Python
+# The time module provides various functions to manipulate time values.
+#  It allows you to work with time-related tasks, such as getting the current time, formatting time,
+#  and introducing delays in your code.
+
+
+import time
+
+print(time.time())
+
+print(4)
+
+time.sleep(2)
+print("this is after 2 seconds")
+
